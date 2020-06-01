ec14225a36ff2d1bc27507829d1e5ea7c2f2ff98
zhuping54960 <549609146@qq.com>
2020-06-01 18:08:49 +0800

    Day01

--- src/main/java/day01/Day01.java
+++ src/main/java/day01/Day01.java
@@ -1,8 +1,28 @@
 package day01;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+/**
+ * mybatis 接口式编程有着更强得类型检查   解耦
+ */
 public class Day01 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception{
+        String resource = "mybatis.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+
+        try (SqlSession sqlSession=sqlSessionFactory.openSession()){
+            Student student=new Student();
+            student.setId(1);
+            Student result=sqlSession.selectOne("day01.StudentMapper1.selectStudent","2");
+            System.out.println(result);
+        }
     }
 }
---
+++ src/main/java/day01/StudentMapper.java
@@ -0,0 +1,7 @@
+package day01;
+
+public interface StudentMapper {
+
+    public Student selectStudent();
+
+}
